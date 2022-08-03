0019ba650745414d5fe3833f0d3aa0ca7d4205f2
Hezuikn <68823892+Hezuikn@users.noreply.github.com>
2022-08-03 21:42:35 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![feature(type_name_of_val)]
 
-use single_borrow::single_borrow;
+use single_borrow::SingleBorrow;
 
 fn main() {
-    let t = &&&&&&&&&&&&&&&&&&&&&&&&&&&&&42;
+    let t = &&&&&&42;
     dbg!(type_name_of_val(&t));
-    let t: &_ = single_borrow(t);
+    let t = t.single_borrow();
     dbg!(type_name_of_val(&t));
     dbg!(t);
 }
