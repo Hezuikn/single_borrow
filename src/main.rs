8a64adf2d84ce8d68b45fd9f12e65b3374030eee
Hezuikn <68823892+Hezuikn@users.noreply.github.com>
2022-08-04 13:17:08 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 #![feature(type_name_of_val)]
 
-use single_borrow::SingleBorrow;
+use single_borrow::single_borrow;
 
 fn main() {
     let t = &&&&&&42;
     dbg!(type_name_of_val(&t));
-    let t = t.single_borrow();
+    let t = single_borrow(t);
     dbg!(type_name_of_val(&t));
-    //broke
-    //dbg!(t);
+    dbg!(t);
 }
 
 use std::any::type_name_of_val;
